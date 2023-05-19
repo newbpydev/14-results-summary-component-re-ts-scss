7b4d84b41382086ddda2c1eadde83b995e8abe8a
Juan Gomez <94386361+newbpydev@users.noreply.github.com>
2023-05-18 23:06:52 -0300

    refactor(SummaryCardStatItem.tsx): remove unused color variables and simplify color assignment style(SummaryCardStats.scss): add hover effect to summary card stats to improve user experience

--- src/components/cards/SummaryCardStatItem.tsx
+++ src/components/cards/SummaryCardStatItem.tsx
@@ -10,19 +10,15 @@ function SummaryCardStatItem({ category, score, icon }: Props) {
   let color: "#ff5555" | "#ffb21e" | "#00bb8f" | "#1125d6";
   switch (category) {
     case "Reaction":
-      // color = "#ff5555";
       colorMix = "red";
       break;
     case "Memory":
-      // color = "#ffb21e";
       colorMix = "yellow";
       break;
     case "Verbal":
-      // color = "#00bb8f";
       colorMix = "green";
       break;
     default:
-      // color = "#1125d6";
       colorMix = "blue";
       break;
   }
@@ -32,9 +28,7 @@ function SummaryCardStatItem({ category, score, icon }: Props) {
       <div className="summary-stat__icon-title">
         <img src={icon} alt={category} className="summary-stat__icon" />
 
-        <h3 className="summary-stat__category" style={{ color: color }}>
-          {category}
-        </h3>
+        <h3 className="summary-stat__category">{category}</h3>
       </div>
 
       <p className="summary-stat__result">
